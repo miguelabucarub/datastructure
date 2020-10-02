1fa4cc98a47f67eeeca182305bb23c41457603f0
miguel.abucarub.neto <miguel.abucarub.neto@CPX-068JEUQBWS3.dir.svc.accenture.com>
2020-10-02 11:28:43 -0300

    Included interface implementation of student

--- src/main/java/com/abucarub/miguel/datastructure/list/Vector.java
+++ src/main/java/com/abucarub/miguel/datastructure/list/Vector.java
@@ -1,9 +1,10 @@
 package com.abucarub.miguel.datastructure.list;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 /**
- * This class stores students in arrays 
+ * This class stores students in arrays
  * 
  * 
  * @author miguel.abucarub.neto
@@ -14,36 +15,88 @@ public class Vector implements ListStudent {
 	// Setting student array with 100 elements
 	private Student[] students = new Student[100];
 
-	public void addStudent(Student student) {
+	private int sumOfStudents = 0;
 
-		
+	private void checkArraySize() {
+
+		if (this.sumOfStudents == this.students.length) {
+			Student[] newArray = new Student[this.students.length * 2];
+			for (int i = 0; i < newArray.length; i++) {
+				newArray[i] = this.students[i];
+			}
+			this.students = newArray;
+		}
+	}
+
+	// add in the end of list
+	public void addStudent(Student student) {
+		this.checkArraySize();
+		long tempoInicial = System.currentTimeMillis();
+		this.students[this.sumOfStudents] = student;
+		this.sumOfStudents++;
+		System.out.println("Time spent was: " + (System.currentTimeMillis() - tempoInicial));
 	}
 
 	public void addStudent(int position, Student student) {
 
-		
+		this.checkArraySize();
+		if (!this.isPositionIsValid(position)) {
+			throw new IllegalArgumentException("Is not a valid position!");
+		}
+
+		for (int i = this.sumOfStudents - 1; i >= position; i -= 1) {
+			this.students[i + 1] = this.students[i];
+		}
+
+		this.students[position] = student;
+		this.sumOfStudents++;
 	}
 
 	public void remove(int position) {
 
-		
+		if (!this.isPositionIsFilled(position)) {
+			throw new IllegalArgumentException("Position filled");
+		}
+
+		for (int i = position; i < this.sumOfStudents - 1; i++) {
+			this.students[i] = this.students[i + 1];
+		}
+		this.sumOfStudents--;
 	}
 
 	public Student retrive(int position) {
 
-		return null;
+		if (!this.isPositionFree(position)) {
+			throw new IllegalArgumentException("Invalid position!!");
+		}
+		return this.students[position];
+	}
+
+	private boolean isPositionIsValid(int position) {
+		return position >= 0 && position <= this.sumOfStudents;
+	}
+
+	private boolean isPositionFree(int position) {
+		return position >= 0 && position < this.size();
+	}
+
+	private boolean isPositionIsFilled(int position) {
+		return position < this.students.length && position >= 0;
 	}
 
 	public boolean contains(Student student) {
-
+		for (int i = 0; i < this.students.length; i++) {
+			if (student.equals(this.students[i])) {
+				return true;
+			}
+		}
 		return false;
 	}
 
-	public void count() {
-
-		
+	public int size() {
+		return this.sumOfStudents;
 	}
-	
+
 	public String toString() {
 		return Arrays.toString(students);
 	}
